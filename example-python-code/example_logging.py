662508252388a88a2cdf3f9e2e0fea1fe0c1707e
xxpasswd <1490621113@qq.com>
2017-12-20 13:47:24 +0800

    add ex_logging

---
+++ example-python-code/example_logging.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.debug("debug message")
+logging.info("info message")
+logging.warning("warning message")
+logging.error("error message")
+logging.critical("critical message")
