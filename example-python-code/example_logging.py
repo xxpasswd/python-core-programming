d85d0d065000aa61fb31fcc4f13e08bdc0d975fd
xxpasswd <1490621113@qq.com>
2017-12-21 13:55:15 +0800

    change logging

--- example-python-code/example_logging.py
+++ example-python-code/example_logging.py
@@ -1,5 +1,10 @@
 import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(pathname)s\n %(filename)s [line:%(lineno)s] %(levelname)s %(message)s',
+                    filename='/tmp/test.log',
+                    filemode='w')
+
 logging.debug("debug message")
 logging.info("info message")
 logging.warning("warning message")
