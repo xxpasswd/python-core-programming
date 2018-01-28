3e630830f5e9b336d780764d6e04515f53d9c53d
xxpasswd <1490621113@qq.com>
2018-01-28 19:47:20 +0800

    add brain_storm.py and job.py

--- brain_storm/brain_storm_first.py
+++ brain_storm/brain_storm_first.py
@@ -1,5 +1,5 @@
 '''
-微信头脑王者辅助(全自动)
+微信头脑王者辅助
 
 mitmproxy -p 手机代理端口 -s brain_storm.py
 '''
@@ -9,49 +9,28 @@ import json
 import requests
 from urllib.parse import quote
 import string
-from redis import Redis
-from rq import Queue
-import time
-from job import auto_touch,next_cycle
-
-# 消息队列，异步处理
-q = Queue(connection=Redis())
-
-
 
 def response(flow):
-	'''
-	捕获服务器reponse，抓取问题和选项，并处理
-	'''
 	path = flow.request.path 
 	if path == "/question/bat/findQuiz":
 		data = json.loads(flow.response.text)
 		question = data["data"]["quiz"]
 		options = data["data"]["options"]
 		ctx.log.info("question: {},options: {}".format(question,options))
-		options,ans_count = get_answer(question,options)
+		options = get_answer(question,options)
 		data["data"]["options"] = options
 		flow.response.text = json.dumps(data)
-		job = q.enqueue(auto_touch,ans_count)
-	elif path == "/question/bat/fightResult":
-		q.enqueue(next_cycle)
-		
-
 
 
 def get_answer(question,options):
-	'''
-	搜寻答案
-	'''
 	url = quote("https://www.baidu.com/s?wd=" + question,safe=string.printable)
 	headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"}
 	r = requests.get(url,headers=headers)
 	content = r.text
 
 	answer = []
-	ans_count = []
 	for option in options:
 		count = content.count(option)
 		answer.append(option+"("+str(count)+")")
-		ans_count.append(count)
-	return answer,ans_count
+
+	return answer
---
+++ brain_storm/job.py
@@ -0,0 +1,36 @@
+import time
+import subprocess
+
+# 点击时间设置
+click_time = 4.5
+next_cycle_time = 8
+
+def auto_touch(op):
+    '''
+    自到寻找最优答案，选择答案
+    '''
+    stand_answer = 3
+    max_count = 0
+    for i,j in enumerate(op):
+        if j > max_count:
+            max_count = j
+            stand_answer = i+1
+
+    click(stand_answer)
+    
+
+def next_cycle():
+    '''
+    一轮结束后，点击下一轮
+    '''
+    time.sleep(next_cycle_time)
+    subprocess.run("adb shell input swipe 520 1400 520 1400 1",shell=True)
+    # subprocess.run("adb shell input swipe 520 800 520 800 1",shell=True)
+
+
+def click(num):
+    '''
+    点击答案
+    '''
+    time.sleep(click_time)
+    subprocess.run("adb shell input swipe 520 {0} 520 {0} 1".format(200*num+840),shell=True)
